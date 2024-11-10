744125ba3d0ce3404ca3029ddbcdf5e2cbbdf663
Jan Hruška <91071872+SpaceIIl@users.noreply.github.com>
2024-11-10 12:05:29 +0100

    feat: Birthdate added to kanban and list view

--- custom_addons/__manifest__.py
+++ custom_addons/__manifest__.py
@@ -7,6 +7,8 @@
     'depends': ['base', 'contacts'],
     'data': [
         'views/res_partner_view.xml',
+        'views/res_partner_list_view.xml',
+        'views/res_partner_kanban_view.xml',
     ],
     'installable': True,
     'application': False,
